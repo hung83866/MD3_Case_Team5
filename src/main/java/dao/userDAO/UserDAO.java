1d96d828501d86bd3583dfb95bc14a513e1c43da
unknown <knts2nttn@gmail.com>
2022-05-20 10:39:22 +0700

    thanh l2

--- src/main/java/dao/userDAO/UserDAO.java
+++ src/main/java/dao/userDAO/UserDAO.java
@@ -2,37 +2,145 @@ package dao.userDAO;
 
 import model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserDAO implements IUserDAO{
-    @Override
-    public void insert(User user) throws SQLException {
+import static config.DBconnection.getConnection;
 
+public class UserDAO implements IUserDAO{
+    private static final String INSERT_USER_SQL = "insert into user(username, email, password, img) VALUES (?, ?, ?, ?);";
+    private static final String SELECT_USER_BY_ID = "select id,username,email,password,img from user where id =?";
+    private static final String SELECT_ALL_USER = "select * from user";
+    private static final String DELETE_USER_SQL = "delete from user where id = ?;";
+    private static final String UPDATE_USER_SQL = "update user set username = ?,email= ?,password= ?,img= ? where id = ?;";
+    private static final String SEARCH_BY_NAME = "select id,username,email,password,img from user where username = ?;";
+
+    public UserDAO() {
     }
 
     @Override
+    public void insert(User user) throws SQLException {
+        System.out.println(INSERT_USER_SQL);
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_SQL)) {
+            preparedStatement.setString(1, user.getUserName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getPassWord());
+            preparedStatement.setString(4,user.getImg());
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+
+    @Override
     public User select(int id) {
-        return null;
+        User user = null;
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);) {
+            preparedStatement.setInt(1, id);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                String username = rs.getString("username");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                String img=rs.getString("img");
+                user = new User(id, username,email,password,img);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return user;
     }
 
     @Override
     public List<User> selectAll() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USER);) {
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String username = rs.getString("username");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                String img = rs.getString("img");
+                users.add(new User(id, username, email, password,img));
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return users;
     }
 
     @Override
     public boolean delete(int id) throws SQLException {
-        return false;
+        boolean rowDeleted;
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE_USER_SQL);) {
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
     }
 
     @Override
     public boolean update(User user) throws SQLException {
-        return false;
+        boolean rowUpdated;
+        try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(UPDATE_USER_SQL);) {
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassWord());
+            statement.setString(4, user.getImg());
+            statement.setInt(5, user.getId());
+
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
     }
 
     @Override
     public List<User> searchByName(String name) {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SEARCH_BY_NAME);) {
+            preparedStatement.setString(1,name);
+            System.out.println(preparedStatement);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String username = rs.getString("username");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                String img = rs.getString("img");
+                users.add(new User(id, username, email, password,img));
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return users;
     }
 }
